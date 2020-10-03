6e862327374fc798a8011ba9d5b5ab9940149c3e
Nikolay <ceo@machineand.me>
2020-10-03 14:01:09 +0300

    UI Kitten

--- App.js
+++ App.js
@@ -1,47 +1,16 @@
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { AppNavigator } from './navigation.component';
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <MainScreen></MainScreen>
-    </View>
-  )
-}
-
-class MainScreen extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-  render() {
-    return (
-      <View>
-        <Text style={styles.title}>Hello bro</Text>
-      </View>
-    )
-  }
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: "#eaeaea"
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: "#20232a",
-    borderRadius: 6,
-    backgroundColor: "#61dafb",
-    color: "#20232a",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold"
-  }
-});
+export default () => (
+  <>
+    <StatusBar style="dark"/>
+    <IconRegistry icons={EvaIconsPack}/>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <AppNavigator/>
+    </ApplicationProvider>
+  </>
+);
